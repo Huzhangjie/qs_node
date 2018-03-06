c3c3374fe53aea99235f7c9481db0fc5a9a257bc
Huzhangjie <1534634433@qq.com>
2018-03-06 22:44:29 +0800

    创建了一个vue项目 更改config文件 使得App.vue中能跨区获取数据

---
+++ yzm/yzm-fe/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" v-key="index">{{user}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+   this.$http.get('gp/api')
+      .then(data => {
+        console.log(data.data.data)
+        this.users = data.data.data
+      })
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
