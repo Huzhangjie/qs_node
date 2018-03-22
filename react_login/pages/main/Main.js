4d5a055b39de70ea60c5179b59c2cc5bbf70eaf1
Huzhangjie <1534634433@qq.com>
2018-03-22 22:45:19 +0800

    main页面

---
+++ react_login/pages/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+class App extends React.Component {
+  render() {
+    return(
+      <div id="app"></div>
+    )
+  }
+}
+
+export default App
